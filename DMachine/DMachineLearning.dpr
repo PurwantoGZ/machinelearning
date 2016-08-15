b6548e290ce430f0bb3467b1406cff67eddc8d10
PurwantoGz <purwanto@outlook.com>
2016-08-15 23:52:28 +0700

    update Project Delphi

---
+++ DMachine/DMachineLearning.dpr
@@ -0,0 +1,13 @@
+program DMachineLearning;
+
+uses
+  Forms,
+  NUnit in 'NUnit.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
